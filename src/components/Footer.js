2d0bcdfbeabb9a16f9684a3013279e247b472087
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-01-30 11:37:09 +0530

    Scrimba Travel Journal

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <small>
+          Coded with ❤️ by{" "}
+          <a
+            className="footer--link"
+            href="bit.ly/priyamoghe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Priya Moghe
+          </a>
+          .
+        </small>
+      </footer>
+    );
+  }
